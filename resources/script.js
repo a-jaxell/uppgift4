0135a7e52a5fdca507109ffd2e211e888c9bd1e8
KvDread <97404672+KvDread@users.noreply.github.com>
2022-09-27 13:04:58 +0200

    Add counter to completed tasks

--- resources/script.js
+++ resources/script.js
@@ -1,17 +1,21 @@
 // Selectors
 const addBtn = document.querySelector("#addBtn");
 const input = document.querySelector("#input");
-
+let counterNum = 0;
 const counter = document.querySelector("#counter");
 const error = document.querySelector("#error");
 
 const list = document.querySelector("#list");
+const item = document.createElement('li');
+const itemLabel = document.createElement('span');
+
 
 const promptError = "You must enter a task"
 const promptDefault = "";
 
 //Event listerners
 addBtn.addEventListener('click', addBtnClick);
+itemLabel.addEventListener('click', completedToggle);
 
 //Functions
 function addBtnClick(){
@@ -22,13 +26,20 @@ function addBtnClick(){
         error.innerText = promptDefault;
         const text = input.value;
 
-        const item = document.createElement('li');
         list.appendChild(item);
 
-        const itemLabel = document.createElement('span');
         itemLabel.innerText = text;
         item.appendChild(itemLabel);
 
     }
     input.value = "";
+}
+function completedToggle(){
+    if( item.getAttribute('class') == 'completed'){
+        item.setAttribute('class', '');
+        counter.innerText = --counterNum + " Completed tasks";
+    } else {
+        item.setAttribute('class', 'completed');
+        counter.innerText = ++counterNum + " Completed tasks";
+    }
 }
