ef19a74337a2bbaab1de529360bd48eec4492c79
KvDread <97404672+KvDread@users.noreply.github.com>
2022-09-28 12:03:33 +0200

    Add trashcan icon and delete function

--- resources/script.js
+++ resources/script.js
@@ -4,7 +4,6 @@ const input = document.querySelector("#input");
 let counterNum = 0;
 const counter = document.querySelector("#counter");
 const error = document.querySelector("#error");
-
 const list = document.querySelector("#list");
 
 const promptError = "You must enter a task"
@@ -12,7 +11,7 @@ const promptDefault = "";
 
 //Event listerners
 addBtn.addEventListener('click', addBtnClick);
-list.addEventListener('click', completedToggle);
+list.addEventListener('click', handleDeleteOrCheck);
 
 //Functions
 function addBtnClick(){
@@ -28,12 +27,26 @@ function addBtnClick(){
         list.appendChild(item);                       // Appends a item as a child element to the element list
 
         const itemLabel = document.createElement('span'); // Creates a span element and stores it in variable called itemLabel
-        itemLabel.innerText = text;                        // The value of the span element is set to that of the variable text
+        itemLabel.innerText = text;                           // The value of the span element is set to that of the variable text
+        itemLabel.setAttribute('class', 'listItem');
         item.appendChild(itemLabel);                        // appends the span element as a child element of the listitem
         
+        const trashcan = document.createElement('span');
+        trashcan.innerHTML = '&#x1F5D1';
+        trashcan.setAttribute('class', 'trashcan');
+        item.appendChild(trashcan);
     }
     input.value = "";
 }
+function handleDeleteOrCheck(e){
+    if( e.target.getAttribute('class') == 'trashcan'){
+        deleteTodo(e);
+    }
+    if( e.target.getAttribute('class') == 'listItem'){
+    completedToggle(e);
+    }
+}
+
 function completedToggle(e){                // Checks if element has the style class of completed and assigns it and increases the counter
     let item = e.target.parentNode;
     if( item.getAttribute('class') == 'completed'){
@@ -43,4 +56,9 @@ function completedToggle(e){                // Checks if element has the style c
         item.setAttribute('class', 'completed');
         counter.innerText = ++counterNum + " Completed tasks";
     }
+}
+
+function deleteTodo(e){
+    item = e.target.parentNode;
+    item.remove();
 }
